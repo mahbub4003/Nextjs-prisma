8a78c6378833417a7a720e4789938d272ad07c65
mahbub-alam <mahbub01750934003@gmail.com>
2023-11-04 07:51:49 +0600

    mhmm

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
-import FeatureBlog from "./components/FeatureBlog";
 import BlogsList from "./components/BlogsList";
 import Subcribe from "./components/Subcribe";
+import FeatureBlogs from "./components/FeatureBlogs";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <FeatureBlog />
+      <FeatureBlogs />
       <BlogsList />
       <Subcribe />
     </>
